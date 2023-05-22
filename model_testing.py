7c05738dff2ac2f25522af93b26995e5c0f67830
Vladimir Katin <katin.v.v@gmail.com>
2023-05-22 23:29:26 +0300

    add accuracy

--- model_testing.py
+++ model_testing.py
@@ -19,4 +19,15 @@ for filename in os.listdir("test"):
 # Проверка модели на данных из папки "test"
 for i, data in enumerate(test_data):
     predicted = model.predict(data.reshape(-1, 1))
-    print(f"Для файла test/data_{i+1}.txt предсказанное значение: {predicted[0]}")
+    print(f"Для файла test/data_{i + 1}.txt предсказанное значение: {predicted[0]}")
+
+# Convert the test data to a numpy array
+test_data = np.array(test_data)
+
+# Predict labels for the test data
+predicted_labels = model.predict(test_data[:, 0].reshape(-1, 1))
+
+# Calculate accuracy
+accuracy = model.score(test_data[:, 0].reshape(-1, 1), predicted_labels)
+
+print(f"Model test accuracy is: {accuracy:.3f}")
