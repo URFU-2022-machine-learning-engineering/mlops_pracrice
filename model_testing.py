566671dac2fdeb80ac8d1ce01f73dfc560753e85
Vladimir Katin <katin.v.v@gmail.com>
2023-05-22 23:24:06 +0300

    have accuracy

--- model_testing.py
+++ model_testing.py
@@ -2,12 +2,12 @@ import os
 import pickle
 import numpy as np
 
-# Загрузка модели из файла
+# Load the model from file
 model_filename = "model.pkl"
 with open(model_filename, "rb") as file:
     model = pickle.load(file)
 
-# Загрузка данных из папки "test"
+# Load the test data from the "test" folder
 test_data = []
 
 for filename in os.listdir("test"):
@@ -16,7 +16,27 @@ for filename in os.listdir("test"):
         data = np.loadtxt(filepath, delimiter=",")
         test_data.append(data)
 
-# Проверка модели на данных из папки "test"
-for i, data in enumerate(test_data):
-    predicted = model.predict(data.reshape(-1, 1))
-    print(f"Для файла test/data_{i+1}.txt предсказанное значение: {predicted[0]}")
+# Convert the test data to a numpy array
+test_data = np.array(test_data)
+
+# Predict labels for the test data
+predicted_labels = model.predict(test_data[:, 0].reshape(-1, 1))
+
+# Calculate accuracy
+accuracy = model.score(test_data[:, 0].reshape(-1, 1), predicted_labels)
+
+print(f"Model test accuracy is: {accuracy:.3f}")
+
+# Generate ground truth labels for the test data
+ground_truth_labels = model.predict(test_data[:, 0].reshape(-1, 1))
+
+# Save ground truth labels to file
+ground_truth_folder = "ground_truth"
+os.makedirs(ground_truth_folder, exist_ok=True)
+
+for i, label in enumerate(ground_truth_labels):
+    filename = f"ground_truth_label_{i+1}.txt"
+    filepath = os.path.join(ground_truth_folder, filename)
+    np.savetxt(filepath, label.reshape(1,), delimiter=",")
+
+print("Ground truth labels for the test data have been created.")
